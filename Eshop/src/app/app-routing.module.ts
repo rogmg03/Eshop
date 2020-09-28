f7300bcc4e28213e113a92e8dd956c91602ce7b1
Roger <rogermora03@gmail.com>
2020-09-27 18:43:59 -0600

    Routing fixed
    Home and login routing added

--- Eshop/src/app/app-routing.module.ts
+++ Eshop/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginviewComponent} from "./loginview/loginview.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: "full",redirectTo: "/home"},
+  {path: "home", component: HomeComponent},
+  {path: "login", component: LoginviewComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
