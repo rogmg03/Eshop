0ac3e4c103213d47905b24d42893d2d10058275f
Roger <rogermora03@gmail.com>
2020-10-09 00:42:12 -0600

    New views
    Nuevas vistas:
    -Login
    -Contactanos
    -Productos
    -Carrito de compras
    -Detalle de producto
    -Confirmacion de compra
    -Pago

--- Eshop/src/app/app-routing.module.ts
+++ Eshop/src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginviewComponent} from "./loginview/loginview.component";
 import {HomeComponent} from "./home/home.component";
+import {ContactComponent} from "./contact/contact.component"
+import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
+import {ProductsComponent} from "./products/products.component";
+import {PaymentComponent} from "./payment/payment.component";
+import {ConfirmComponent} from "./confirm/confirm.component";
+import {ProductDetailComponent} from "./product-detail/product-detail.component";
 
 const routes: Routes = [
   {path: "", pathMatch: "full",redirectTo: "/home"},
+  {path: "contact", component: ContactComponent},
   {path: "home", component: HomeComponent},
   {path: "login", component: LoginviewComponent},
+  {path: "products", component: ProductsComponent},
+  {path: "shop", component: ShoppingCartComponent},
+  {path: "payment", component: PaymentComponent},
+  {path: "confirm", component: ConfirmComponent},
+  {path: "product-detail", component: ProductDetailComponent},
 ];
 
 @NgModule({
--- Eshop/src/app/app.module.ts
+++ Eshop/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginviewComponent } from './loginview/loginview.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginviewComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent,
+    ShoppingCartComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    ConfirmComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
