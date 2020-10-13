8d9eb075cf75cdf150be3d91c156bf0834f47dc5
Jonathan García Ugalde <jonagar@hotmail.com>
2020-10-13 01:14:29 -0600

    some changes

--- RestService/Controllers/FarmersController.cs
+++ RestService/Controllers/FarmersController.cs
@@ -10,6 +10,7 @@ namespace Controllers{
         
         [Route ("api/farmers")]
         [HttpGet]
+        // este metodo se encarga de enviar todos  los agritucores registrados
         public IEnumerable<Farmers> FarmerInfo(){
             int size = getSize();
             Farmers[] farmers = new Farmers[size];
@@ -26,11 +27,38 @@ namespace Controllers{
             return farmers;
         }
 
+
+        [Route("api/CountUsers")]
+        [HttpGet]
+        
         public int getSize(){
             string json = System.IO.File.ReadAllText(@"./JsonDataBase/farmers.json");
             dynamic array = JsonConvert.DeserializeObject(json);
             return array.Count;
         }
+
+        [Route("api/names")]
+       [HttpGet]
+
+        public IEnumerable<string> FarmerNames()
+        {
+            int size = getSize();
+            string [] farmers = new string[size];
+            string json = System.IO.File.ReadAllText(@"./JsonDataBase/farmers.json");
+            dynamic array = JsonConvert.DeserializeObject(json);
+            // List<Farmers> farmers = new List<Farmers>();
+            // foreach(var element in array){    
+            //     farmers.Add(farmer);
+            // } 
+            for (int index = 0; index < size; index++)
+            {
+                Farmers farmer = JsonConvert.DeserializeObject<Farmers>(array[index].ToString());
+                farmers[index] = farmer.name;
+            }
+            return farmers;
+        }
+
     }
+    
 
 }
